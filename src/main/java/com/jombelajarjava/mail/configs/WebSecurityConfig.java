df2a75d49983f2a4782b84228786f55884490b9e
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-04-15 10:38:30 +0800

    Chane x-frame-options to allow-from

--- src/main/java/com/jombelajarjava/mail/configs/WebSecurityConfig.java
+++ src/main/java/com/jombelajarjava/mail/configs/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package com.jombelajarjava.mail.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.StaticAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${jombelajarjava.baseurl}")
+    private String baseUrl;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .headers()
                 .frameOptions()
-                .sameOrigin()
+                .disable()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(new StaticAllowFromStrategy(new URI(baseUrl))))
                 .and()
                 .authorizeRequests()
                 .antMatchers("/**")
